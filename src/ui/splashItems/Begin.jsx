3148372344c1167d6a79c15c4857da83d6ef5a5d
Jon <jon.bent@me.com>
2021-07-21 12:06:38 -0700

    fixes github garden, updates react-scripts

--- src/ui/splashItems/Begin.jsx
+++ src/ui/splashItems/Begin.jsx
@@ -1,20 +1,15 @@
 import React, {Component} from 'react';
 import Arrow from "../../svg/Arrow";
-import GitHubCalendar from 'github-calendar'
-import '../../scss/garden.css';
+import GitHubCalendar from 'github-calendar';
+import '../../scss/garden.scss';
 class Begin extends Component {
     componentDidMount() {
         GitHubCalendar('.garden', 'jonbent', {responsive: true}).then(() => {
-            document.querySelectorAll('rect[fill="#ebedf0"]').forEach(el => el.style.fill = "rgba(0,0,0,0.5)");
-            document.querySelector('li[style="background-color: #ebedf0"]').style.backgroundColor = "rgba(0,0,0,0.5)";
-            document.querySelectorAll('rect[fill="#c6e48b"]').forEach(el => el.style.fill = "#BFCDE0");
-            document.querySelector('li[style="background-color: #c6e48b"]').style.backgroundColor = "#BFCDE0";
-            document.querySelectorAll('rect[fill="#7bc96f"]').forEach(el => el.style.fill = "#5D5D81");
-            document.querySelector('li[style="background-color: #7bc96f"]').style.backgroundColor = "#5D5D81";
-            document.querySelectorAll('rect[fill="#239a3b"]').forEach(el => el.style.fill = "#3B3355");
-            document.querySelector('li[style="background-color: #239a3b"]').style.backgroundColor = "#3B3355";
-            document.querySelectorAll('rect[fill="#196127"]').forEach(el => el.style.fill = "#2f2842");
-            document.querySelector('li[style="background-color: #196127"]').style.backgroundColor = "#2f2842";
+            document.querySelectorAll('rect[data-level="0"]').forEach(el => el.style.fill = "rgba(0,0,0,0.5)");
+            document.querySelectorAll('rect[data-level="4"]').forEach(el => el.style.fill = "#BFCDE0");
+            document.querySelectorAll('rect[data-level="3"]').forEach(el => el.style.fill = "#5D5D81");
+            document.querySelectorAll('rect[data-level="2"]').forEach(el => el.style.fill = "#3B3355");
+            document.querySelectorAll('rect[data-level="1"]').forEach(el => el.style.fill = "#2f2842");
         });
     }
 
